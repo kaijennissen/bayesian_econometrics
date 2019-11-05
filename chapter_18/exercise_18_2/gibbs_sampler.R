b19905729e9e94c1a715e4bc26c4252af277f3e7
Kai Jennissen <kai.jennissen@statworx.com>
2019-11-05 09:14:31 +0100

    fixed error in conditional for sigma2_c

--- chapter_18/exercise_18_2/gibbs_sampler.R
+++ chapter_18/exercise_18_2/gibbs_sampler.R
@@ -5,28 +5,30 @@
 # Cambridge: Cambridge University Press.
 # -----------------------------------------------------------------------------
 library(Matrix)
+library(svMisc)
+library(progress)
 
 rm(list = ls())
-data <- read.csv("./chapter_18/exercise_18_2/USPCE_2015Q4.csv", header = FALSE)
+#data <- read.csv("./chapter_18/exercise_18_2/USPCE_2015Q4.csv", header = FALSE)
+data <- read.csv("./chapter_18/exercise_18_2/usgdp.csv", header = FALSE)
 y <- ts(data = data, start = c(1959, 1), freq = 4)
 y <- c(log(y)) * 100
 y <- as.matrix(y)
 colnames(y) <- NULL
 TT <- dim(y)[1]
-p=2
+p <- 2
 
 #-------------------
 # priors
 #-------------------
-
 # phi
 V_phi <- diag(2)
 invV_phi <- solve(V_phi)
 phi_0 <- matrix(c(1.3, -0.7))
 
 # gamma
-V_gamma <- 100*diag(2)
-gamma_0 <- matrix(c(750,750))
+V_gamma <- 100 * diag(2)
+gamma_0 <- matrix(c(750, 750))
 invV_gamma <- solve(V_gamma)
 
 # sigma2_tau
@@ -36,30 +38,33 @@ b_sigma2_tau <- 0.01
 ny_sigma2_c <- 3
 S_sigma2_c <- 2
 
-
+#-------------------
 # initla values
+#-------------------
 phi <- matrix(c(1.34, -0.7))
 sigma2_c <- .5
 sigma2_tau <- .001
 tau0 <- matrix(c(y[1], y[1]))
 
-# construct time invariant matrices
 # H2
-diags <- list(rep(1, TT), rep(-2, TT - 1), rep(1, TT-2))
+diags <- list(rep(1, TT), rep(-2, TT - 1), rep(1, TT - 2))
 H2 <- bandSparse(TT, k = c(0, -1, -2), diag = diags, symm = FALSE)
 invH_2 <- solve(H2)
-HH_2 <- as.matrix(t(H2)%*%H2)
+HH_2 <- t(H2) %*% H2
 
 # H_phi
-diags_phi <- list(rep(1, TT), rep(phi[1], TT - 1), rep(phi[2], TT-2))
+diags_phi <- list(rep(1, TT), rep(phi[1], TT - 1), rep(phi[2], TT - 2))
 H_phi <- bandSparse(TT, k = c(0, -1, -2), diag = diags_phi, symm = FALSE)
-HH_phi <- as.matrix(t(H_phi)%*%H_phi)
+HH_phi <- t(H_phi) %*% H_phi
 
 # X_gamma
-X_gamma <- cbind(c(2:(TT+1)), -c(1:TT))
+X_gamma <- cbind(c(2:(TT + 1)), -c(1:TT))
 
 # alpha_tau_tilde
-alpha_tau_tilde <- matrix(c(2*tau0[2]-tau0[1],-tau0[2], rep(0,TT-2)))
+alpha_tau_tilde <- matrix(c(2 * tau0[2] - tau0[1], -tau0[2], rep(0, TT - 2)))
+
+
+
 
 nsim <- 1e4
 nburn <- 1e3
@@ -73,80 +78,93 @@ store_phi <- matrix(0, nrow = nsim, ncol = 2)
 n_grid <- 500
 count_phi <- 0
 
-bar_steps <- (nsim + nburn) / 100
+step_size <- (nsim + nburn) / 100
+pb <- progress_bar$new(
+  format = "[:bar] :percent in :elapsed",
+  total = nsim+nburn, clear = FALSE, width = 60
+)
+
 for (ii in 1:(nsim + nburn)) {
+    if(ii==1) cat("\014")
+    pb$tick()
+    Sys.sleep(1 / 100)
 
 
   # draw from conditional for tau
-  alpha_tau <- invH_2%*% alpha_tau_tilde
+  alpha_tau <- invH_2 %*% alpha_tau_tilde
   K_tau <- HH_phi / sigma2_c + HH_2 / sigma2_tau
   L_tau <- chol(K_tau)
-  XX_tau <- (HH_phi %*% y)/sigma2_c + (HH_2 %*% alpha_tau)/sigma2_tau
+  XX_tau <- (HH_phi %*% y) / sigma2_c + (HH_2 %*% alpha_tau) / sigma2_tau
   tau_hat <- solve(K_tau, XX_tau)
   Z_tau <- matrix(rnorm(n = TT, mean = 0, sd = 1))
   tau <- tau_hat + solve(L_tau) %*% Z_tau
 
+
   # draw from conditional for phi
-  c <- y-tau
-  X_phi <- cbind(c(0, c[1:(TT-1)]), c(0,0,c[1:(TT-2)]))
-  XX_phi <- t(X_phi)%*%X_phi
-  K_phi <- invV_phi+XX_phi/sigma2_c
+  c <- y - tau
+  X_phi <- cbind(c(0, c[1:(TT - 1)]), c(0, 0, c[1:(TT - 2)]))
+  XX_phi <- t(X_phi) %*% X_phi
+  K_phi <- invV_phi + XX_phi / sigma2_c
   L_phi <- t(chol(K_phi))
-  XX <- invV_phi%*%phi_0+t(X_phi)%*%c/sigma2_c
+  XX <- invV_phi %*% phi_0 + t(X_phi) %*% c / sigma2_c
   phi_hat <- solve(K_phi, XX)
   Z_phi <- matrix(rnorm(n = p, mean = 0, sd = 1))
-  phic <- phi_hat+solve(L_phi)%*%Z_phi
+  phic <- phi_hat + solve(L_phi) %*% Z_phi
   # stationarity region for AR(2)
   # 1) phi_1 + phi_2 < 1
-  # 2) 
-  if (sum(phic) < .99 && phic[2]- phic[1] < .99 && phic[2] > -.99){
-    phi <-  phic
+  # 2)
+  if (sum(phic) < .99 && phic[2] - phic[1] < .99 && phic[2] > -.99) {
+    phi <- phic
     # calculate H_phi in every iteration
-    diags_phi <- list(rep(1, TT), rep(phi[1], TT - 1), rep(phi[2], TT-2))
+    diags_phi <- list(rep(1, TT), rep(phi[1], TT - 1), rep(phi[2], TT - 2))
     H_phi <- bandSparse(TT, k = c(0, -1, -2), diag = diags, symm = FALSE)
-    HH_phi <- as.matrix(t(H_phi)%*%H_phi)
+    HH_phi <- as.matrix(t(H_phi) %*% H_phi)
     count_phi <- count_phi + 1
   }
-  
+
+
   # draw from condition for sigma^2_c
-  S_sigma2_c_temp <- S_sigma2_c + 0.5 * t(y- tau) %*% HH_phi %*% (y- tau)
-  omega2 <- 1 / rgamma(n = 1, shape = ny_sigma2_c + TT / 2, scale = 1 / as.matrix(S_sigma2_c_temp))
-  
+  S_sigma2_c_temp <- S_sigma2_c + 0.5 * t(y - tau) %*% HH_phi %*% (y - tau)
+  sigma2_c <- 1 / rgamma(n = 1, shape = ny_sigma2_c + TT / 2, scale = 1 / as.matrix(S_sigma2_c_temp))
+
+
   # draw from conditional for sigma^2_tau
-  del_tau <- c(tau0[1], tau[1:TT])-c(tau0[2],tau0[1],tau[1:(TT-1)])
-  f_tau <- function(x){-TT/2*log(x)-sum(diff(del_tau)[-TT]**2)/(2*x)}
-  #sum((del_tau[2:TT]-del_tau[1:(TT-1)])**2)
-  sigma2_tau_grid <- seq(from = runif(1)/1000,to = b_sigma2_tau-runif(1)/1000,length.out = 500)
+  del_tau <- c(tau0[1], tau[1:TT]) - c(tau0[2], tau0[1], tau[1:(TT - 1)])
+  f_tau <- function(x) {
+    -TT / 2 * log(x) - sum(diff(del_tau)[-TT]**2) / (2 * x)
+  }
+  # sum((del_tau[2:TT]-del_tau[1:(TT-1)])**2)
+  sigma2_tau_grid <- seq(from = runif(1) / 1000, to = b_sigma2_tau - runif(1) / 1000, length.out = n_grid)
   lp_sigtau2 <- f_tau(sigma2_tau_grid)
-  p_sigtau2 <-  exp(lp_sigtau2-max(lp_sigtau2))
-  p_sigtau2 <-  p_sigtau2/sum(p_sigtau2)
-  cdf_sigtau2 <-  cumsum(p_sigtau2)
-  sigma2_tau <- sigma2_tau_grid[which(runif(1)<cdf_sigtau2, 1)[1]]
-  
+  p_sigtau2 <- exp(lp_sigtau2 - max(lp_sigtau2))
+  p_sigtau2 <- p_sigtau2 / sum(p_sigtau2)
+  cdf_sigtau2 <- cumsum(p_sigtau2)
+  sigma2_tau <- sigma2_tau_grid[which(runif(1) < cdf_sigtau2, TRUE)[1]]
+
 
   # draw from conditional for gamma
-  K_gamma <- invV_gamma + t(X_gamma)%*%HH_2%*%X_gamma/sigma2_tau
+  K_gamma <- invV_gamma + t(X_gamma) %*% HH_2 %*% X_gamma / sigma2_tau
   L_gamma <- chol(K_gamma)
-  XX <- invV_gamma%*%gamma_0+t(X_gamma)%*%HH_2%*%X_gamma
+  XX <- invV_gamma %*% gamma_0 + t(X_gamma) %*% HH_2 %*% tau / sigma2_tau
   gamma_hat <- solve(K_gamma, XX)
   Z_gamma <- rnorm(n = p, mean = 0, sd = 1)
-  gamma <- gamma_hat+solve(L_gamma)%*%Z_gamma
-  
-  
-  progress(ii / bar_steps)
-  if (ii == (nsim + nburn)) cat(": Done")
+  gamma <- gamma_hat + solve(L_gamma) %*% Z_gamma
+
 
   if (ii > nburn) {
     nn <- ii - nburn
-    store_tau[nn, ] <- tau[,, drop = T]
-    store_phi[nn, ] <- phi[, , drop = T]
-    store_gamme[nn, ] <- gamma[, , drop = T]
+    store_tau[nn, ] <- tau@x
+    store_phi[nn, ] <- phi@x
+    store_gamma[nn, ] <- gamma@x
     store_sigma2_tau[nn] <- sigma2_tau
     store_sigma2_c[nn] <- sigma2_c
   }
 }
 
+
 tau_post <- colMeans(store_tau)
-sigma2_post <- colMeans(store_sigma)
-omega2_post <- colMeans(store_omega)
-tau0_post <- colMeans(store_tau0)
+phi_post <- colMeans(store_phi)
+gamma_post <- colMeans(store_gamma)
+sigma2_tau_post <- colMeans(store_sigma2_tau)
+sigma2_c_post <- colMeans(store_sigma2_c)
+

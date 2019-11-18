284af547b16ca28c58f3b72ab9330c5ef81bb2ab
KJ <kai.jennissen@statworx.com>
2019-11-18 15:52:53 +0100

    added plot

--- chapter_18/exercise_18_2/gibbs_sampler.R
+++ chapter_18/exercise_18_2/gibbs_sampler.R
@@ -1,4 +1,4 @@
-# -----------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 # R Code for Exercise 18.2
 # Chan, J., Koop, G., Poirier, D.J. and Tobias, J.L. (2019).
 # Bayesian Econometric Methods (2nd edition).
@@ -31,9 +31,8 @@ now <- Sys.time()
 TT <- dim(y)[1]
 p <- 2
 
-#-------------------
-# priors
-#-------------------
+
+# priors #-----------------------------------------------------------------------------------------
 # phi
 phi_0 <- matrix(c(1.3, -0.7))
 V_phi <- diag(2)
@@ -50,6 +49,7 @@ b_sigma2_tau <- 0.01
 # sigma2_c
 ny_sigma2_c <- 3
 S_sigma2_c <- 2
+
 
 # initial values #---------------------------------------------------------------------------------
 
@@ -87,14 +87,6 @@ HH_phi <- t(H_phi) %*% H_phi
 # X_gamma
 X_gamma <- cbind(c(2:(TT + 1)), -c(1:TT))
 
-f_tau <- function(x, del_tau, TT) {
-  -TT / 2 * log(x) - sum(diff(del_tau)[-TT]**2) / (2 * x)
-}
-
-
-nsim <- 1e4
-nburn <- 1e3
-total_runs <- nsim+nburn
 
 store_tau <- matrix(0, nrow = nsim, ncol = TT)
 store_gamma <- matrix(0, nrow = nsim, ncol = 2)
@@ -105,17 +97,16 @@ store_phi <- matrix(0, nrow = nsim, ncol = 2)
 n_grid <- 500
 count_phi <- 0
 
-step_size <- total_runs / 100
+alpha_tau_tilde <- matrix(0, nrow = TT)
 
 
 for (ii in 1:total_runs) {
   # draw from conditional for tau #----------------------------------------------------------------
   # gamma = (tau(0), tau(-1))'
-  alpha_tau_tilde <-
-    matrix(c(2 * gamma[1] - gamma[2], -gamma[1], rep(0, TT - 2))) 
+  alpha_tau_tilde[1, 1] <- 2 * gamma[1] - gamma[2]
+  alpha_tau_tilde[2, 1] <- -gamma[1]
   alpha_tau <- invH_2 %*% alpha_tau_tilde
   K_tau <- HH_phi / sigma2_c + HH_2 / sigma2_tau
-  tau <- tau_hat + solve(L_tau) %*% Z_tau
   L_tau <- chol(K_tau)
   XX_tau <-
     (HH_phi %*% y) / sigma2_c + (HH_2 %*% alpha_tau) / sigma2_tau
@@ -124,11 +115,11 @@ for (ii in 1:total_runs) {
   tau <-
     tau_hat + solve(L_tau, diag(ncol(L_tau))) %*% Z_tau # for large matrizes solve (XX, diag(ncol(XX))) is faster
   
-
-  # draw from conditional for phi
-  c <- y - tau
-  X_phi <- cbind(c(0, c[1:(TT - 1)]), c(0, 0, c[1:(TT - 2)]))
-  XX_phi <- t(X_phi) %*% X_phi
+  
+  # draw from conditional for phi #----------------------------------------------------------------
+  cc <- y - tau
+  X_phi <- cbind(c(0, cc[1:(TT - 1)]), c(0, 0, cc[1:(TT - 2)]))
+  XX_phi <- crossprod(X_phi)
   K_phi <- invV_phi + XX_phi / sigma2_c
   L_phi <- chol(K_phi) #
   XX <- invV_phi %*% phi_0 + crossprod(X_phi, cc) / sigma2_c
@@ -168,16 +159,14 @@ for (ii in 1:total_runs) {
   # gamma = (tau(0), tau(-1))'
   del_tau <-
     c(gamma[1], tau[1:TT]) - c(gamma[2], gamma[1], tau[1:(TT - 1)])
-  # f_tau <- function(x) {
-  #   -TT / 2 * log(x) - sum(diff(del_tau)[-TT] ** 2) / (2 * x)
-  # }
   sigma2_tau_grid <-
     seq(
       from = runif(1) / 1000,
       to = b_sigma2_tau - runif(1) / 1000,
       length.out = n_grid
     )
-  lp_sigtau2 <- f_tau(x = sigma2_tau_grid, TT = TT, del_tau = del_tau)
+  lp_sigtau2 <-
+    f_tau(x = sigma2_tau_grid, TT = TT, del_tau = del_tau)
   p_sigtau2 <- exp(lp_sigtau2 - max(lp_sigtau2))
   p_sigtau2 <- p_sigtau2 / sum(p_sigtau2)
   cdf_sigtau2 <- cumsum(p_sigtau2)
@@ -193,15 +182,19 @@ for (ii in 1:total_runs) {
   gamma_hat <- solve(K_gamma, XX)
   Z_gamma <- rnorm(n = p, mean = 0, sd = 1)
   gamma <- gamma_hat + solve(L_gamma) %*% Z_gamma
+  
+  
+  # store #----------------------------------------------------------------------------------------
   if (ii > nburn) {
     nn <- ii - nburn
-    store_tau[nn,] <- tau@x
-    store_phi[nn,] <- phi@x
-    store_gamma[nn,] <- gamma@x
+    store_tau[nn, ] <- tau@x
+    store_phi[nn, ] <- phi@x
+    store_gamma[nn, ] <- gamma@x
     store_sigma2_tau[nn] <- sigma2_tau
     store_sigma2_c[nn] <- sigma2_c
   }
 }
+print(Sys.time() - now)
 
 # Results #----------------------------------------------------------------------------------------
 tau_hat <- colMeans(store_tau)
@@ -210,15 +203,15 @@ gamma_hat <- colMeans(store_gamma)
 sigma2_tau_hat <- colMeans(store_sigma2_tau)
 sigma2_c_hat <- colMeans(store_sigma2_c)
 
-theta_hat <- matrix(c(c(phi_hat),sigma2_c_hat, sigma2_tau_hat, c(gamma_hat) ))
+theta_hat <-
+  matrix(c(c(phi_hat), sigma2_c_hat, sigma2_tau_hat, c(gamma_hat)))
 
 cc <- ts(y - tau_hat, start = c(1949, 1), frequency = 4)
 plot(cc)
 
 
-timetk::tk_tbl(data = cc, rename_index = "time") %>% 
-  rename(c = 'Series 1') %>% 
-  ggplot(aes(x=time, y = c))+
+timetk::tk_tbl(data = cc, rename_index = "time") %>%
+  rename(c = 'Series 1') %>%
+  ggplot(aes(x = time, y = c)) +
   geom_line()
-
 

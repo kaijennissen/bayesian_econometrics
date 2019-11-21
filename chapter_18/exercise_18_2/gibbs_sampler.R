27ff1fea25017c4eac2c9f32494c9f164b3f871c
KJ <kai.jennissen@statworx.com>
2019-11-21 18:44:37 +0100

    added crossprod

--- chapter_18/exercise_18_2/gibbs_sampler.R
+++ chapter_18/exercise_18_2/gibbs_sampler.R
@@ -4,7 +4,7 @@
 # Bayesian Econometric Methods (2nd edition).
 # Cambridge: Cambridge University Press.
 #------------------------------------------------------------------------------
-# run time: 155 sec
+# run time: 138 sec
 
 library(Matrix)
 library(dplyr)
@@ -17,12 +17,11 @@ nsim <- 10000
 nburn <- 1000
 total_runs <- nsim + nburn
 
-data <- read.csv("./chapter_18/exercise_18_2/usgdp.csv", header = FALSE)
-y <- ts(
-    data = data,
-    start = c(1959, 1),
-    freq = 4
-)
+data <-
+    read.csv("./chapter_18/exercise_18_2/usgdp.csv", header = FALSE)
+y <- ts(data = data,
+        start = c(1959, 1),
+        freq = 4)
 y <- c(log(y)) * 100
 y <- as.matrix(y)
 colnames(y) <- NULL
@@ -53,7 +52,7 @@ S_sigma2_c <- 2
 # initial values #-------------------------------------------------------------
 
 f_tau <- function(x, TT, del_tau) {
-    -TT / 2 * log(x) - sum(diff(del_tau)[-TT]**2) / (2 * x)
+    -TT / 2 * log(x) - sum(diff(del_tau)[-TT] ** 2) / (2 * x)
 }
 
 
@@ -64,15 +63,41 @@ gamma <- matrix(c(y[1], y[1])) # !! gamma = (tau(0), tau(-1))'
 
 # H_2
 diags_h2 <- list(rep(1, TT), rep(-2, TT - 1), rep(1, TT - 2))
-H_2 <- bandSparse(TT, k = c(0, -1, -2), diag = diags_h2, symm = FALSE)
-invH_2 <- solve(H_2)
-HH_2 <- t(H_2) %*% H_2
+H_2 <-
+    bandSparse(TT,
+               k = c(0, -1, -2),
+               diag = diags_h2,
+               symm = FALSE)
 
+microbenchmark::microbenchmark(
+    bandSparse(
+        TT,
+        k = c(0, -1, -2),
+        diag = diags_h2,
+        symm = FALSE
+    ),
+    Diagonal(TT) + sparseMatrix(2:TT,  1:(TT - 1), x = rep(-2, TT - 1), dims = c(TT, TT)) +
+        sparseMatrix(3:TT,  1:(TT - 2), x = rep(1, TT - 2), dims = c(TT, TT)),
+    times = 1000,
+    unit = "s"
+)
+
+
+invH_2 <- solve(H_2, diag(TT))
+HH_2 <- crossprod(H_2)
+
+microbenchmark::microbenchmark(crossprod(H_2), t(H_2) %*% H_2)
 
 # H_phi
-diags_phi <- list(rep(1, TT), rep(-phi[1], TT - 1), rep(-phi[2], TT - 2))
-H_phi <- bandSparse(TT, k = c(0, -1, -2), diag = diags_phi, symm = FALSE)
-HH_phi <- t(H_phi) %*% H_phi
+diags_phi <-
+    list(rep(1, TT), rep(-phi[1], TT - 1), rep(-phi[2], TT - 2))
+H_phi <-
+    bandSparse(TT,
+               k = c(0, -1, -2),
+               diag = diags_phi,
+               symm = FALSE)
+#HH_phi <- t(H_phi) %*% H_phi
+HH_phi <- crossprod(H_phi)
 
 # X_gamma
 X_gamma <- cbind(c(2:(TT + 1)), -c(1:TT))
@@ -94,12 +119,13 @@ for (ii in 1:total_runs) {
     alpha_tau <- invH_2 %*% alpha_tau_tilde
     K_tau <- HH_phi / sigma2_c + HH_2 / sigma2_tau
     L_tau <- chol(K_tau)
-    XX_tau <- (HH_phi %*% y) / sigma2_c + (HH_2 %*% alpha_tau) / sigma2_tau
+    XX_tau <-
+        (HH_phi %*% y) / sigma2_c + (HH_2 %*% alpha_tau) / sigma2_tau
     tau_hat <- solve(K_tau, XX_tau)
     Z_tau <- matrix(rnorm(n = TT, mean = 0, sd = 1))
     tau <- tau_hat + solve(L_tau, diag(ncol(L_tau))) %*% Z_tau
-
-
+    
+    
     # draw from conditional for phi #------------------------------------------
     cc <- y - tau
     X_phi <- cbind(c(0, cc[1:(TT - 1)]), c(0, 0, cc[1:(TT - 2)]))
@@ -114,41 +140,64 @@ for (ii in 1:total_runs) {
     # 1) phi_1 + phi_2 < 1
     # 2) phi_2 - phi_1 < 1
     # 3) phi_2 > -1
-    if (sum(phic) < .99 && phic[2] - phic[1] < .99 && phic[2] > -.99) {
+    if (sum(phic) < .99 &&
+        phic[2] - phic[1] < .99 && phic[2] > -.99) {
         phi <- phic
         # calculate H_phi in every iteration
-        diags_phi <- list(rep(1, TT), rep(-phi[1], TT - 1), rep(-phi[2], TT - 2))
-        H_phi <- bandSparse(TT, k = c(0, -1, -2), diag = diags_phi, symm = FALSE)
-        HH_phi <- t(H_phi) %*% H_phi
+        diags_phi <-
+            list(rep(1, TT), rep(-phi[1], TT - 1), rep(-phi[2], TT - 2))
+        H_phi <-
+            bandSparse(TT,
+                       k = c(0, -1, -2),
+                       diag = diags_phi,
+                       symm = FALSE)
+        HH_phi <- crossprod(H_phi)
         count_phi <- count_phi + 1
     }
-
-
+    
+    
     # draw from condition for sigma^2_c #--------------------------------------
     S_sigma2_c_temp <- S_sigma2_c + 0.5 * t(cc) %*% (HH_phi %*% cc)
-    sigma2_c <- 1 / rgamma(n = 1, shape = ny_sigma2_c + TT / 2, scale = 1 / S_sigma2_c_temp@x)
-
-
+    sigma2_c <-
+        1 / rgamma(
+            n = 1,
+            shape = ny_sigma2_c + TT / 2,
+            scale = 1 / S_sigma2_c_temp@x
+        )
+    
+    
     # draw from conditional for sigma^2_tau #---------------------------------------------------------
     # gamma = (tau(0), tau(-1))'
-    del_tau <- c(gamma[1], tau[1:TT]) - c(gamma[2], gamma[1], tau[1:(TT - 1)])
-    sigma2_tau_grid <- seq(from = runif(1) / 1000, to = b_sigma2_tau - runif(1) / 1000, length.out = n_grid)
-    lp_sigtau2 <- f_tau(x = sigma2_tau_grid, TT = TT, del_tau = del_tau)
+    del_tau <-
+        c(gamma[1], tau[1:TT]) - c(gamma[2], gamma[1], tau[1:(TT - 1)])
+    sigma2_tau_grid <-
+        seq(
+            from = runif(1) / 1000,
+            to = b_sigma2_tau - runif(1) / 1000,
+            length.out = n_grid
+        )
+    lp_sigtau2 <-
+        f_tau(x = sigma2_tau_grid,
+              TT = TT,
+              del_tau = del_tau)
     p_sigtau2 <- exp(lp_sigtau2 - max(lp_sigtau2))
     p_sigtau2 <- p_sigtau2 / sum(p_sigtau2)
     cdf_sigtau2 <- cumsum(p_sigtau2)
-    sigma2_tau <- sigma2_tau_grid[which(runif(1) < cdf_sigtau2, TRUE)[1]]
-
-
+    sigma2_tau <-
+        sigma2_tau_grid[which(runif(1) < cdf_sigtau2, TRUE)[1]]
+    
+    
     # draw from conditional for gamma #----------------------------------------
-    K_gamma <- invV_gamma + t(X_gamma) %*% (HH_2 %*% X_gamma) / sigma2_tau
+    K_gamma <-
+        invV_gamma + t(X_gamma) %*% (HH_2 %*% X_gamma) / sigma2_tau
     L_gamma <- chol(K_gamma)
-    XX <- invV_gamma %*% gamma_0 + t(X_gamma) %*% (HH_2 %*% tau) / sigma2_tau
+    XX <-
+        invV_gamma %*% gamma_0 + t(X_gamma) %*% (HH_2 %*% tau) / sigma2_tau
     gamma_hat <- solve(K_gamma, XX)
     Z_gamma <- rnorm(n = p, mean = 0, sd = 1)
     gamma <- gamma_hat + solve(L_gamma) %*% Z_gamma
-
-
+    
+    
     # store #------------------------------------------------------------------
     if (ii > nburn) {
         nn <- ii - nburn
@@ -170,5 +219,5 @@ cc <- ts(y - tau_hat, start = c(1949, 1), frequency = 4)
 print(theta_hat)
 timetk::tk_tbl(data = cc, rename_index = "time") %>%
     rename(c = "Series 1") %>%
-    ggplot(aes(x = time, y = c)) +
+    ggplot2::ggplot(aes(x = time, y = c)) +
     geom_line()

1eca23c90a06f6839a2fdd996ee5ba027c7b4c07
Kai Jennissen <kai.jennissen@statworx.com>
2020-01-13 12:56:09 +0100

    v0.1

--- papers/Chan_Jeliazkov_2009/kalman_filter.R
+++ papers/Chan_Jeliazkov_2009/kalman_filter.R
@@ -8,7 +8,7 @@
 
 library(Matrix)
 library(SuppDists)
-#library(RcppZiggurat)
+library(RcppZiggurat)
 library(Rcpp)
 
 
@@ -24,7 +24,7 @@ library(Rcpp)
 # 
 
 rm(list = ls())
-now <- Sys.time()
+
 
 set.seed(123)
 
@@ -38,19 +38,17 @@ y <- ts(data = data,
         start = c(1948, 1),
         freq = 4)
 
-# ggplot2::ggplot(reshape2::melt(y), ggplot2::aes(x = Var1, y=value, col=Var2))+
-#     ggplot2::geom_line()
-
 #y <- c(log(y)) * 100
 y <- as.matrix(y)
 colnames(y) <- NULL
 y0 <- y[1:3, ]
-y <- y[-c(1:3), ]
+#y <- y[-c(1:3), ]
 
-Y <- c(t(y))
+Y <- matrix(c(t(y[-c(1:3),])))
 
-TT <- nrow(y)
+tt <- nrow(y)
 nn <- ncol(y)
+TT <- nrow(Y)/nn
 qq <- nn*(nn+1)
 
 # priors #---------------------------------------------------------------------
@@ -72,7 +70,7 @@ Omega11 <- Matrix(cov(y))
 Omega11_inv <- Matrix(solve(Omega11, diag(ncol(Omega11))))
 
 # H
-diags_H <- list(rep(1, TT*qq), rep(1, (TT - 1)*qq))
+diags_H <- list(rep(1, TT*qq), rep(-1, (TT - 1)*qq))
 H <- bandSparse(n = TT*qq,
                k = c(0, -qq),
                diag = diags_H,
@@ -90,11 +88,11 @@ S_inv <- bdiag(replicate((TT-1), Omega22_inv, simplify = F))
 S_inv <-  bdiag(DD_inv, S_inv)  
 
 # bigG = X
-G <- bdiag(replicate(qq, matrix(c(1, y[3:(TT-1),]), ncol=nn+1), simplify = F))
+#G <- bdiag(replicate(qq, matrix(c(1, y[3:(TT-1),]), ncol=nn+1), simplify = F))
 
-G_list <- vector("list", TT)
-for (i in 1:TT){
-G_list[[i]] <- kronecker(diag(4), matrix(c(1, y[i,]), ncol=nn+1))
+G_list <- vector("list", 245)
+for (i in 3:247){
+    G_list[[i-2]] <- kronecker(diag(4), matrix(c(1, y[i,]), ncol=nn+1))
 }
 G <- bdiag(G_list)
 
@@ -107,19 +105,18 @@ store_Omega22 <- array(NA, dim=c(nsim, qq))
 
 # Gibbs Sampler -----------------------------------------------------------
 for (ii in 1:total_runs) {
-
+    now <- Sys.time()
 # beta --------------------------------------------------------------------
     S_inv <- bdiag(replicate((TT-1), Omega22_inv, simplify = F))
     S_inv <-  bdiag(DD_inv, S_inv)  
-    K <- crossprod(H, S_inv) %*% H
+    K <- Matrix::forceSymmetric(crossprod(H, S_inv) %*% H)
     G_Omega11_inv <- crossprod(G, kronecker(diag(TT), Omega11_inv))
-    G_Omega11_inv_G <- G_Omega11_inv %*% G
-    P <- Matrix::forceSymmetric(K + G_Omega11_inv_G)
+    G_Omega11_inv_G <-  Matrix::forceSymmetric(G_Omega11_inv %*% G)
+    P <- K + G_Omega11_inv_G
     L <- chol(P)
-    
-    beta_hat <- backsolve(L, forwardsolve(L, G_Omega11_inv  %*% Y, upper.tri = FALSE), upper.tri = TRUE,
-              transpose = TRUE)
-    beta <- beta_hat + solve(L, rnorm(n = length(beta_hat)))
+    beta_hat <- Matrix(backsolve(L, forwardsolve(L, G_Omega11_inv  %*% Y, upper.tri = FALSE),
+                          upper.tri = TRUE, transpose = TRUE))
+    beta <- beta_hat + solve(L, zrnorm(n = length(beta_hat)))
     
 # Omega_11 ----------------------------------------------------------------
     e1 <- matrix(Y-G%*%beta, nrow=nn)
@@ -127,15 +124,14 @@ for (ii in 1:total_runs) {
     Omega11_inv <- rWishart(n = 1, df = nu1 + TT, Sigma =  solve(new_S1, diag(nn)))[,,1]
     Omega11 <- solve(Omega11, diag(nn))
 
-    
 # Omega_22 |---------------------------------------------------------------
     e2 <- matrix(H%*%beta, ncol=qq)
     new_S2 <- (S2 + colSums(e2[-1,]**2))/2
-     diag(Omega22) <- 1/rgamma(n=20,
+    diag(Omega22) <- 1/rgamma(n=20,
                  shape = new_nu2,
                  scale = 1/new_S2
                      )
-    
+    print(Sys.time() - now)
 # store |------------------------------------------------------------------
     if (ii > nburn) {
         run <- ii - nburn
@@ -144,7 +140,7 @@ for (ii in 1:total_runs) {
         store_Omega22[run, ] <- diag(Omega22)
     }
 }
-print(Sys.time() - now)
+
 
 
 # Results #--------------------------------------------------------------------
@@ -153,6 +149,26 @@ Omega11_hat <- apply(store_Omega11, MARGIN = c(1,2), FUN = mean)
 Omega22_hat <- colMeans(store_Omega22)
 
 y_hat <- matrix(beta_hat, nrow=qq)
+XX <- array(NA, c(4,5,245))
+for (i in 1:245){
+    XX[,,i] <- matrix(y_hat[,i], nrow=nn)
+}
+
+
+dimnames(XX) <- list(NULL, c("mu", "gdp", "une", "int", "inf"), NULL)
+gdp <- t(XX[1,,])
+une <- t(XX[2,,])
+int <- t(XX[3,,])
+inf <- t(XX[4,,])
+#reshape2::melt(gdp)
+
+ggplot2::ggplot(reshape2::melt(inf),
+    ggplot2::aes(x = Var1, y = value, col=Var2)) +
+    ggplot2::geom_line()+
+    ggplot2::ylim(c(-0.4,0.4))
+
+
+
 
 
 

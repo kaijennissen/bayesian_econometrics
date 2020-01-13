4178f642584cc35705108b59322a34de799e111a
Kai Jennissen <kai.jennissen@statworx.com>
2020-01-13 14:56:13 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-INPUT_DIR=$BASE_DIR/bayesian_econometric_methods
-
-docker run -it \
- --rm \
- -v $INPUT_DIR:/home/docker \ 
---name bayes \
-nuest/mro:latest
+docker run -e PASSWORD=RockerDocker \
+  --rm \
+  -p 8787:8787 \
+  -v $BASE_DIR:/home/rstudio \
+  --name bayes \
+  bayes:0.1

b8471b85fa8c0ef7c2db4390b7c84d2d29cae811
Kai Jennissen <kai.jennissen@statworx.com>
2020-01-29 16:30:22 +0100

    clean up

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker run -e PASSWORD=RockerDocker \
-  --rm \
-  -p 8787:8787 \
-  -v $BASE_DIR:/home/rstudio \
-  --name bayes \
-  bayes:0.1
